a39efb6aefdabdca3cf24f5e6d1ac44ca68b8425
unknown <kakidiakoafonso@gmail.com>
2021-12-29 22:52:58 +0100

    Fix option bug 2 time

--- src/Server.ts
+++ src/Server.ts
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', "true");
     // handle OPTIONS method
     if ('OPTIONS' == req.method) {
         return res.sendStatus(200);
